902ba3841f41d13a16b01066e0a2de6f30110dd6
Antonio Perez <antonio.perez62@educa.madrid.org>
2026-09-17 13:07:25 +0200

    Refactorizacion: nueva clase MessageManager

--- src/es/ciudadescolar/Programa.java
+++ src/es/ciudadescolar/Programa.java
@@ -1,6 +1,10 @@
 package es.ciudadescolar;
+import es.ciudadescolar.utils.MessageManager;
+
 public class Programa {
     public static void main(String[] args) throws Exception {
-        System.out.println("Empezamos a usar Repositorios de Código…");
+        MessageManager mm = new MessageManager();
+
+        System.out.println(mm.getSaludo());
     }
 }
---
+++ src/es/ciudadescolar/utils/MessageManager.java
@@ -0,0 +1,9 @@
+package es.ciudadescolar.utils;
+
+public class MessageManager {
+    private final String saludo = "Empezamos a usar Repositorios de Código…";
+
+    public String getSaludo(){
+        return saludo;
+    }
+}
